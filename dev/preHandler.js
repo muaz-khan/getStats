f97eed375ed5a62297ed2a374b59d415e53e1839
arthur <arthur.wangc@dadaabc.com>
2018-10-19 14:18:53 +0800

    fix: indexOf

--- dev/preHandler.js
+++ dev/preHandler.js
@@ -7,8 +7,12 @@ function preHandler(result) {
     }, {});
     // 若与原有字段有冲突，则在其他代码中做值检测，并用其他字段进行判断。例如sendrecvType
     return result.reduce(function(sum, item) {
+        if (item.type != 'outbound-rtp' && item.type != 'inbound-rtp' && item.type.indexOf('candidate') < 0) {
+            sum.push(item);
+            return sum;
+        }
         // 兼容candidate相关参数 - 处理字段兼容，转义
-        if (item.type.indexof('candidate') >= 0) {
+        if (item.type.indexOf('candidate') >= 0) {
             if (item.ip) {
                 item.ipAddress = item.ip;
             }
@@ -22,10 +26,6 @@ function preHandler(result) {
                 item.portNumber = item.port;
             }
         };
-        if (item.type != 'outbound-rtp' && item.type != 'inbound-rtp' && item.type.indexof('candidate') < 0) {
-            sum.push(item);
-            return sum;
-        }
         item = Object.assign(item, idMap[item.transportId], idMap[item.codecId], idMap[item.trackId]);
         if (item.mimeType) {
             item.googCodecName = item.mimeType.split('/')[1];
